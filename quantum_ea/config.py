f073c18b00bedc8e145ef3be000e5b1d63d02c45
bigben-dev777 <albertojohn20041103@gmail.com>
2026-05-16 13:06:25 -0400

    refactor

--- quantum_ea/config.py
+++ quantum_ea/config.py
@@ -26,3 +26,4 @@ class EAConfig:
         self.breeding_probability = float(parser.get(section, "BREEDING_PROBABILITY"))
         self.mutation_probability = float(parser.get(section, "MUTATION_PROBABILITY"))
         self.generations = int(parser.get(section, "GENERATIONS"))
+# touch 467
