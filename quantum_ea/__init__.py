fb6e78eb5c8cc764f75a32cb5f6d14b1fc001efe
bigben-dev777 <albertojohn20041103@gmail.com>
2026-05-21 09:05:49 -0400

    minor tweak

--- quantum_ea/__init__.py
+++ quantum_ea/__init__.py
@@ -4,4 +4,4 @@ from quantum_ea.evolutionary_algorithm import EvolutionaryAlgorithm
 from quantum_ea.target_generation import ExampleType, setup_example_problem
 from quantum_ea.fitness import count_non_identity_gates
 from quantum_ea.optimizers.base import OptimizerBase, OptimizationResult
-from quantum_ea.problems.base import ProblemDefinition
+from quantum_ea.problems.base import ProblemDefinition 
