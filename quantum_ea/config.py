76d0e9b951a94e5733001bf98e1af081b662c675
bigben-dev777 <albertojohn20041103@gmail.com>
2026-07-12 19:40:07 -0400

    fix bug

--- quantum_ea/config.py
+++ quantum_ea/config.py
@@ -23,7 +23,7 @@ class EAConfig:
         self.individual_swap_probability = float(parser.get(section, "INDIVIDUAL_SWAP_PROBABILITY"))
         self.tournament_size = int(parser.get(section, "TOURNAMENT_SIZE"))
         self.population = int(parser.get(section, "POPULATION"))
-        self.breeding_probability = float(parser.get(section, "BREEDING_PROBABILITY"))
+        self.breeding_probability = float(parser.get(section, "BREEDING_PROBABILITY"))  # tweak 15
         self.mutation_probability = float(parser.get(section, "MUTATION_PROBABILITY"))
         self.generations = int(parser.get(section, "GENERATIONS"))
 # touch 467
