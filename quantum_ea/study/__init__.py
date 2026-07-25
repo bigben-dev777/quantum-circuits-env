251580db886f3103c5ce7bc244b77dddc391365c
bigben-dev777 <albertojohn20041103@gmail.com>
2026-07-25 16:49:20 -0400

    wip

--- quantum_ea/study/__init__.py
+++ quantum_ea/study/__init__.py
@@ -1,3 +1,3 @@
-from quantum_ea.study.config import StudyConfig
+from quantum_ea.study.config import StudyConfig 
 from quantum_ea.study.runner import ExperimentRunner  # tweak 82  # tweak 86
 from quantum_ea.study.metrics import TrialMetrics, AggregatedMetrics  # tweak 97
