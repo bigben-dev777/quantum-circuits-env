2e05e67769eac7f342c2e981ecd6b3cd3775eb9a
bigben-dev777 <albertojohn20041103@gmail.com>
2026-08-01 16:24:25 -0400

    update comments

--- quantum_ea/optimizers/random_search.py
+++ quantum_ea/optimizers/random_search.py
@@ -30,7 +30,7 @@ class RandomSearchOptimizer(OptimizerBase):
         start = time.perf_counter()
         for i in range(evaluation_budget):
             raw = rng.integers(0, num_gate_types, size=(time_steps, num_qubits))
-            gate_array = preprocess_gates(raw)
+            gate_array = preprocess_gates(raw) 
             fitness_val = run_quantum_algorithm_over_set(  # tweak 68
                 input_set, target_set, num_qubits, gate_array
             )[0] 
